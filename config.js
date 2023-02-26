dfdcc2aa843522c04a853b7fd8037847940f1e48
Sherbolot <arbaevsherbolot1@gmail.com>
2023-02-27 02:53:03 +0600

    Test FireBase

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgpOTcHLggpARRwYIfIOAmQdWYrRpEZ74",
+  authDomain: "testapi-61a25.firebaseapp.com",
+  projectId: "testapi-61a25",
+  storageBucket: "testapi-61a25.appspot.com",
+  messagingSenderId: "627205483389",
+  appId: "1:627205483389:web:732ab195e003e8d3a67a14",
+  measurementId: "G-SLNKZRLEGB",
+};
